20d52f72ee1e484c78722828edfd92fe31cd8c7e
sengwoong <rej287573@gmail.com>
2024-12-18 03:27:06 +0900

    :sparkles: 메뉴 이미지 업로드

--- src/navigations/stack/HomeHeaderLeft.tsx
+++ src/navigations/stack/HomeHeaderLeft.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { DrawerActions, useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Image } from 'react-native';
 
 function HomeHeaderLeft() {
-
-    const navigation = useNavigation(); 
+    const navigation = useNavigation();
 
     return (
-       <TouchableOpacity 
-         onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
-          <Icon name="menu" size={32} color="black" />
-       </TouchableOpacity>
-     );
+        <TouchableOpacity 
+            onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+            style={{ padding: 10 }}
+        >
+            <Image 
+                source={require('../../asset/images/Menu.png')}
+                style={{ width: 24, height: 24 }}
+            />
+        </TouchableOpacity>
+    );
 }
-    
+
 export default HomeHeaderLeft;
